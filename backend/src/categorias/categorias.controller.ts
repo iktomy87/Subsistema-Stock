ab612de4ed5e8acec62f944599d3149fbfe7d6c0
Alejandro Torres <at7709739@gmail.com>
2025-10-22 23:42:37 -0300

    feat: scopes agregados a todas las rutas

--- backend/src/categorias/categorias.controller.ts
+++ backend/src/categorias/categorias.controller.ts
@@ -8,33 +8,42 @@ import {
   Param, 
   HttpCode, 
   HttpStatus,
-  ParseIntPipe 
+  ParseIntPipe, 
+  UseGuards
 } from '@nestjs/common';
 import { CategoriasService } from './categorias.service';
 import { CreateCategoriaDto } from './dto/create-categoria.dto';
 import { UpdateCategoriaDto } from './dto/update-categoria.dto';
 import { Categoria } from './entities/categoria.entity';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
+import { ScopesGuard } from '../auth/scopes.guard';
+import { Scopes } from '../auth/scopes.decorator';
 
 @Controller('categorias')
+@UseGuards(JwtAuthGuard, ScopesGuard)
 export class CategoriasController {
   constructor(private readonly categoriasService: CategoriasService) {}
 
   @Get()
+  @Scopes('categorias:read')
   async findAll(): Promise<Categoria[]> {
     return this.categoriasService.findAll();
   }
 
   @Get(':id')
+  @Scopes('categorias:read')
   async findOne(@Param('id', ParseIntPipe) id: number): Promise<Categoria> {
     return this.categoriasService.findOne(id);
   }
 
   @Post()
+  @Scopes('categorias:write')
   async create(@Body() createCategoriaDto: CreateCategoriaDto): Promise<Categoria> {
     return this.categoriasService.create(createCategoriaDto);
   }
 
   @Patch(':id')
+  @Scopes('categorias:write')
   async update(
     @Param('id', ParseIntPipe) id: number,
     @Body() updateCategoriaDto: UpdateCategoriaDto,
@@ -43,6 +52,7 @@ export class CategoriasController {
   }
 
   @Delete(':id')
+  @Scopes('categorias:write')
   @HttpCode(HttpStatus.NO_CONTENT)
   async remove(@Param('id', ParseIntPipe) id: number): Promise<void> {
     return this.categoriasService.remove(id);
