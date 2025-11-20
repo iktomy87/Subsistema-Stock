d7ad641c25e7fa0723c9ef6a4319db6912c879b9
Facundo Cubells <facundocubells@gmail.com>
2025-11-20 16:07:31 -0300

    finale

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,9 +1,7 @@
 import { getSession } from 'next-auth/react';
 import { PaginatedProducts, Category, Product, ReservaInput, ReservaOutput, ReservaCompleta, CancelacionReservaInput, ActualizarReservaInput, ProductoInput, ProductoUpdate, ProductoCreado, CategoriaInput, PaginatedReservas } from './definitions';
 
-const API_BASE_URL = typeof window === 'undefined' 
-    ? process.env.NEXT_PUBLIC_API_URL || 'http://backend:3000' 
-    : '/api';
+const API_BASE_URL = process.env.NEXT_PUBLIC_STOCK_API_URL;
 
 interface Session {
     accessToken?: string;
